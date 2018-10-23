5a12137f7fc7fb2c2bcf60cc7648332fa853b22c
YannHyaric <ylhalmeida@gmail.com>
2018-10-23 20:57:26 -0300

    criação do Adapter para listagem de eventos e participantes

--- app/src/main/java/com/example/ylhal/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ylhal/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.ylhal.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button evento, participante;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        participante = findViewById(R.id.btn_participante);
+        evento = findViewById(R.id.btn_evento);
+        rv = findViewById(R.id.Listas);
+
+        evento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Evento.class);
+
+            }
+        });
+
+        participante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ylhal/myapplication/MainAdapter.java
@@ -0,0 +1,25 @@
+package com.example.ylhal.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
