b702401789ae68bfaccddfbe52d956a128d12230
YannHyaric <ylhalmeida@gmail.com>
2018-10-23 20:35:05 -0300

    atribuição das funcionalidades do layout para atributos em cadastro de evento e participante

--- app/src/main/java/com/example/ylhal/myapplication/Evento.java
+++ app/src/main/java/com/example/ylhal/myapplication/Evento.java
@@ -2,12 +2,28 @@ package com.example.ylhal.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Evento extends Activity {
+    private Button cadastrar;
+    private TextView titulo, hora, data, descricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
+        cadastrar = findViewById(R.id.btn_Cadastrar);
+        titulo = findViewById(R.id.editTitulo);
+        hora = findViewById(R.id.editHora);
+        data = findViewById(R.id.editData);
+        descricao = findViewById(R.id.editDesc);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ylhal/myapplication/Participante.java
+++ app/src/main/java/com/example/ylhal/myapplication/Participante.java
@@ -2,12 +2,27 @@ package com.example.ylhal.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Participante extends Activity {
+    private Button cadastrar;
+    private TextView nome, email, cpf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participante);
+        cadastrar = findViewById(R.id.btn_cadastrar);
+        nome = findViewById(R.id.editNome);
+        email = findViewById(R.id.editEmail);
+        cpf = findViewById(R.id.editCPF);
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
