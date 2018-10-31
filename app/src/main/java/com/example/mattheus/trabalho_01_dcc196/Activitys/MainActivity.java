1c2ddd834f3f3e0206c5abc4f175ffdfc261797e
YannHyaric <ylhalmeida@gmail.com>
2018-10-31 03:28:22 -0300

    Atualização das funcionalidades do MainActivity

--- app/src/main/java/com/example/mattheus/trabalho_01_dcc196/Activitys/MainActivity.java
+++ app/src/main/java/com/example/mattheus/trabalho_01_dcc196/Activitys/MainActivity.java
@@ -8,65 +8,108 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.mattheus.trabalho_01_dcc196.Adapters.ListarEventosAdapter;
+import com.example.mattheus.trabalho_01_dcc196.Adapters.ListarParticipantesAdapter;
+import com.example.mattheus.trabalho_01_dcc196.Objetos.Evento;
+import com.example.mattheus.trabalho_01_dcc196.Objetos.Participante;
 import com.example.mattheus.trabalho_01_dcc196.R;
 
 public class MainActivity extends Activity {
 
-    public static final int REQUEST_CADASTRO_PARTICIPANTE = 1;
-    public static final int REQUEST_CADASTRO_EVENTO = 2;
-    public static final int REQUEST_LISTAR_EVENTO = 3;
+    public static final int REQUEST_CADASTRO_EVENTO = 1;
+    public static final int REQUEST_CADASTRO_PARTICIPANTE = 2;
+    public static final int REQUEST_DETALHES_PARTICIPANTE = 3;
     public static final String POSICAO_PARTICIPANTE = "POS";
-
-//    public static final String NOME_PARTICPANTE = "NOME";
-//    public static final String EMAIL_PARTICPANTE = "EMAIL";
-//    public static final String CPF_PARTICPANTE = "CPF";
-//
-//    public static final String TITULO_EVENTO = "TITULO";
-//    public static final String DESCRICAO_EVENTO = "DESCRICAO";
-//    public static final String FACILITADOR = "FACILITADOR";
-//    public static final String DATA = "DATA";
-//    public static final String HORA = "HORA";
+    public static final String NOME_PARTICPANTE = "NOME";
+    public static final String EMAIL_PARTICPANTE = "EMAIL";
+    public static final String CPF_PARTICPANTE = "CPF";
+    public static final String TITULO_EVENTO = "TITULO";
+    public static final String DESCRICAO_EVENTO = "DESCRICAO";
+    public static final String FACILITADOR = "FACILITADOR";
+    public static final String DATA = "DATA";
+    public static final String HORA = "HORA";
+    private RecyclerView rvMain;
+    private Button cadparticipante, cadevento, listeventos;
+    private ListarParticipantesAdapter particpanteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_cadastrar_evento = findViewById(R.id.btn_Cadastrar_Evento);
-        Button btn_cadastrar_participante = findViewById(R.id.btn_Cadastrar_Participante);
-        Button btn_listar_participante = findViewById(R.id.btn_View_Participantes);
 
-        RecyclerView recyclerView = findViewById(R.id.rcl_View_Eventos);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ListarEventosAdapter adapter = new ListarEventosAdapter(Singleton.getInstance().getEventos());
-        recyclerView.setAdapter(adapter);
+        rvMain = findViewById(R.id.rcl_View_Eventos);
 
+        rvMain.setLayoutManager(new LinearLayoutManager(this));
 
+        particpanteAdapter = new ListarParticipantesAdapter(Singleton.getInstance().getParticipantes());
 
-        btn_cadastrar_participante.setOnClickListener(new View.OnClickListener() {
+        rvMain.setAdapter(particpanteAdapter);
+
+        cadparticipante = findViewById(R.id.btn_Cadastrar_Participante);
+
+        particpanteAdapter.setOnParticipanteClickListener(new ListarParticipantesAdapter.OnParticipanteClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CadastrarParticipanteActivity.class);
-                startActivityForResult(intent, MainActivity.REQUEST_CADASTRO_PARTICIPANTE);
+            public void onParticipanteClick(View view, int position) {
+                Intent intent = new Intent(MainActivity.this, EditarParticipante.class);
+                intent.putExtra(MainActivity.POSICAO_PARTICIPANTE, position);
+                startActivityForResult(intent, REQUEST_DETALHES_PARTICIPANTE);
+            }
+
+            @Override
+            public void onLongParticipanteClick(View view, int position) {
+                Singleton.getInstance().removeParticipanteEventos(Singleton.getInstance().getParticipantes().get(position));
+                Singleton.getInstance().removeParticipante(Singleton.getInstance().getParticipantes().get(position));
+                particpanteAdapter.notifyItemRemoved(position);
             }
         });
 
-        btn_cadastrar_evento.setOnClickListener(new View.OnClickListener() {
+        cadparticipante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CadastrarParticipanteActivity.class);
+                startActivityForResult(intent, REQUEST_CADASTRO_PARTICIPANTE);
+
+            }
+        });
+
+        cadevento = findViewById(R.id.btn_Cadastrar_Evento);
+        cadevento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CadastrarEventoActivity.class);
-                startActivityForResult(intent, MainActivity.REQUEST_CADASTRO_EVENTO);
+                startActivityForResult(intent, REQUEST_CADASTRO_EVENTO);
             }
         });
 
-        btn_listar_participante.setOnClickListener(new View.OnClickListener() {
+        listeventos = findViewById(R.id.btn_View_Participantes);
+        listeventos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListarParticipantesActivity.class);
-                startActivityForResult(intent, MainActivity.REQUEST_LISTAR_EVENTO);
+                Intent intent = new Intent(MainActivity.this, ListarEventos.class);
+                startActivity(intent);
             }
         });
+
+
     }
 
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MainActivity.REQUEST_CADASTRO_PARTICIPANTE && resultCode == Activity.RESULT_OK && data != null) {
+            Bundle bundleResultado = data.getExtras();
+            Participante p = new Participante(bundleResultado.getString(MainActivity.NOME_PARTICPANTE), bundleResultado.getString(MainActivity.EMAIL_PARTICPANTE), bundleResultado.getString(MainActivity.CPF_PARTICPANTE));
+            Singleton.getInstance().addParticipante(p);
+
+            particpanteAdapter.notifyDataSetChanged();
+
+        } else if (requestCode == MainActivity.REQUEST_CADASTRO_EVENTO && resultCode == Activity.RESULT_OK && data != null) {
+
+            Bundle bundleResultado = data.getExtras();
+            Evento e = new Evento(bundleResultado.getString(MainActivity.TITULO_EVENTO), bundleResultado.getString(MainActivity.DATA), bundleResultado.getString(MainActivity.HORA), bundleResultado.getString(MainActivity.FACILITADOR), bundleResultado.getString(MainActivity.DESCRICAO_EVENTO));
+            Singleton.getInstance().addEvento(e);
+        }
+    }
 }
